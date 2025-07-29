9f43430acecfa3de522f709ecc2415aca6f6d3cc
Simeon-prog <simeonstoltz@gmail.com>
2025-07-29 22:31:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ module.exports = {
 
         // custom 
         name: "coderxsa", // custom owner name
-        id: "27696024815" // owner numebr
+        id: "2799999999" // owner numebr
     },
 };
 
